0e9402c7d6e922998570948115cceedeeb46ed61
Bratan2009 <matxramoff@gmail.com>
2024-12-18 16:25:18 +0300

    Grade 1. Этап 1: Задание 2
    date_changer.py

---
+++ date_changer.py
@@ -0,0 +1,10 @@
+username = "Bratan@2009"
+title = "Name"
+content = "My name"
+status = "True"
+created_date = "13-12-2024"
+issue_date = "23-12-2024"
+temp_issue_date = "23-12"
+temp_created_date = "13-12"
+sep=(" \n ")
+print("  Дата создания:",temp_created_date,sep, "Дата истечения заметки (дедлайн):", temp_issue_date   )
