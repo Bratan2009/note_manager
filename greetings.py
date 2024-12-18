c46a4ae2b8d3c996b665ed658c51ac29290b37b7
Bratan2009 <matxramoff@gmail.com>
2024-12-18 16:24:14 +0300

    Grade 1. Этап 1: Задание 1
     greetings.py

---
+++ greetings.py
@@ -0,0 +1,9 @@
+username = "Bratan@2009"
+title = "Name"
+content = "My name"
+status = "True"
+created_date = "13-12-2024"
+issue_date = "23-12-2024"
+sep='\n'
+print(" Имя пользователя:", username,sep,"Заголовок заметки:", title ,sep,"Описание заметки:", content,sep,"Статус заметки:", status,sep,
+      "Дата создания заметки:", created_date,sep,"Дата истечения заметки (дедлайн):", issue_date)
