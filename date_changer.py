b5fde24ff9083c64b2034eb43286b1ebc1f92f21
Bratan2009 <matxramoff@gmail.com>
2024-12-19 21:10:33 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -2,9 +2,7 @@ username = "Bratan@2009"
 title = "Name"
 content = "My name"
 status = "True"
-created_date = "13-12-2024"
-issue_date = "23-12-2024"
-temp_issue_date = issue_date.replace("-2024","")
-temp_created_date = created_date.replace("-2024","")
+created_date =input("Введите дату создания заметки в формате день-месяц-год (пример - 13-12-2024):")
+issue_date = input("Введите дату итечения (дедлайна) заметки формате день-месяц-год (пример - 23-12-2024)")
 sep=(" \n ")
-print("  Дата создания:",temp_created_date,sep, "Дата истечения заметки (дедлайн):", temp_iss
+print("  Дата создания:",created_date[:5],sep, "Дата истечения заметки (дедлайн):", issue_date[:5]   )
