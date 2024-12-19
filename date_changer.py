d1baeaae5340ebfcabb72f6cb2b319e9476c99c3
Bratan2009 <matxramoff@gmail.com>
2024-12-19 18:46:30 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -4,7 +4,7 @@ content = "My name"
 status = "True"
 created_date = "13-12-2024"
 issue_date = "23-12-2024"
-temp_issue_date = "23-12"
-temp_created_date = "13-12"
+temp_issue_date = issue_date.replace("-2024","")
+temp_created_date = created_date.replace("-2024","")
 sep=(" \n ")
-print("  Дата создания:",temp_created_date,sep, "Дата истечения заметки (дедлайн):", temp_issue_date   )
+print("  Дата создания:",temp_created_date,sep, "Дата истечения заметки (дедлайн):", temp_iss
